889b017c7f765a5c2af5e198b85bc05422d910a4
Alfux <FUCHSAL@groupe.schmidt>
2025-02-13 15:10:19 +0100

    Cleanup

--- scripts/TGBTRegression.py
+++ scripts/TGBTRegression.py
@@ -18,31 +18,23 @@ def main() -> int:
         av.add_argument("file", help="file with timestamp and IEA")
         av = av.parse_args()
         data: DataFrame = pd.read_csv(av.file).loc[:, ["IEA", "Temps (sec)"]]
-        # relu = Neuron("Neuron.ReLU", "Neuron.dReLU")
         lrelu = Neuron("Neuron.LReLU", "Neuron.dLReLU")
         bias = Neuron("Neuron.bias", "Neuron.dbias")
-        cost = Neuron("Neuron.MSE", "Neuron.dMSE")
-        reg = Teacher(data, "IEA", normal=[-1, 1])
-        reg.mlp = MLP([
+        mlp = MLP([
             Layer([lrelu] * 64 + [bias], np.random.randn(65, 2)),
             Layer([lrelu] * 32 + [bias], np.random.randn(33, 65)),
             Layer([lrelu] * 16 + [bias], np.random.randn(17, 33)),
             Layer([lrelu] * 8 + [bias], np.random.randn(9, 17)),
             Layer([lrelu], np.random.randn(1, 9)),
-            ], cost,
-            learning_rate=1e-3
+            ], Neuron("Neuron.MSE", "Neuron.dMSE")
         )
-        reg.teach(av.epoch).save()
+        Teacher(data, "IEA", normal=[-1, 1], mlp=mlp).teach(av.epoch).save()
         return 0
     except Exception as err:
         if av.debug:
             print(traceback.format_exc())
         print(f"\n\tFatal: {type(err).__name__}: {err}\n", file=sys.stderr)
         return 1
-    except KeyboardInterrupt as err:
-        print(f"\n\t{type(err).__name__}: {err}\n", file=sys.stderr)
-        reg.save()
-        return 2
 
 
 if __name__ == "__main__":
