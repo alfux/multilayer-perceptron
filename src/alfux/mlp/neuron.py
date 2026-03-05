Fixed identity derivative.

src/alfux/mlp/neuron.py
@@ -67,7 +67,7 @@ class Neuron:
     @staticmethod
     def didentity(x: ndarray) -> ndarray:
         """Derivative of the identity function."""
-        return np.ones(x.shape)
+        return np.eye(x.shape[-1])
 
     @staticmethod
     def ReLU(x: ndarray) -> ndarray: